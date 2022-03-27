327c80bc2e62e59d3c2c64102303232e52d6ff7a
Julie Reynal <rjulie.pro@gmail.com>
2022-03-27 10:39:47 +0200

    change string to text to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_082542) do
+ActiveRecord::Schema.define(version: 2022_03_27_083719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
