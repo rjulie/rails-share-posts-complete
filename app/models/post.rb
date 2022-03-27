d25be2d17353a9f2f8601c087422fb0881e14bc7
Julie Reynal <rjulie.pro@gmail.com>
2022-03-27 12:01:06 +0200

    seed and has_many validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  has_many :comments, dependent: :destroy
+  has_many :tags, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Destroying data
+puts "Destroying posts..."
+Post.destroy_all
+puts "Destroying comments..."
+Comment.destroy_all
+puts "Destroying users..."
+User.destroy_all
+puts "Destroying tags..."
+Tag.destroy_all
+
+# Creating users
+puts "Creating users..."
+emails = ["julie@gmail.com", "thomas@gmail.com", "gala@gmail.com", "lucien@gmail.com", "carole@gmail.com"]
+passwords = ["adminj", "admint", "adming", "adminl", "adminc"]
+nicknames = ["julier", "thomasl", "galat", "lucienl", "carolec"]
+
+user_index = 0
+5.times do
+  User.create!(
+    email: emails[user_index],
+    password: passwords[user_index],
+    nickname: nicknames[user_index],
+  )
+  user_index += 1
+end
+puts "Users creation done!"
+
+# Creating Posts
+puts "Creating Posts"
+10.times do
+  Post.create!(
+    title: Faker::Hacker.adjective,
+    content: Faker::Hacker.say_something_smart,
+    url: Faker::Internet.url,
+    user: User.all.sample
+  )
+end
+puts "Posts creation done!"
+
+# Creating Comments
+puts "Creating Comments"
+20.times do
+  Comment.create!(
+    content: Faker::Marketing.buzzwords,
+    user: User.all.sample,
+    post: Post.all.sample
+  )
+end
+puts "Comments creation done!"
+
+# Creating Tags
+puts "Creating Tags"
+categories = ["Rails", "Javascript", "Python", "Flutter", "React"]
+tag_index = 0
+5.times do
+  Tag.create!(
+    category: categories[tag_index],
+    post: Post.all.sample
+  )
+  tag_index += 1
+end
+puts "Tags creation done!"
