063347cd220c13bc0a8224120cdffc2faec769e3
Julie Reynal <rjulie.pro@gmail.com>
2022-03-31 22:28:58 +0200

    comments new and create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    if @comment.save!
+      redirect_to post_path(@post), notice: "Comment has been added"
+    else
+      render "posts/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
+  end
 end
