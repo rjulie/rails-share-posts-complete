76782621f623a1bb68e0b572ba288cc7d95c184d
Julie Reynal <rjulie.pro@gmail.com>
2022-04-08 23:35:47 +0200

    post new in modal

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,28 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :url, :user_id)
   end
 end
